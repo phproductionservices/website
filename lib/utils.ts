5f400b66146feeaca5f1780de3461e4683378c1e
otutuchristopher97 <52664541+otutuchristopher97@users.noreply.github.com>
2025-02-05 15:31:14 +0000

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export type WithClassName<P = unknown> = P & {
-  className?: string;
-};
